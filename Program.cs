845795d7f5c5f24002bdfebd26b735691dd15a18
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-07-12 13:03:03 -0500

    family dictionary, iterate through dictionary of dictionaries

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){ 
+                {"name", "Krista"},
+                {"age", "42"}
+            });
+            myFamily.Add("brother", new Dictionary<string, string>(){ 
+                {"name", "Henry"},
+                {"age", "47"}
+            });
+
+            
+
+            foreach (var dictPair in myFamily) {
+                Console.WriteLine(dictPair.Value["name"] + " is my " + dictPair.Key + " and  is " + dictPair.Value["age"] + " years old");
+            }
+
+
+            
+        }
+    }
+}
